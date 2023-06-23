288130ff7dacd91d544231e0fa19739d7d395248
Shihabun Mobin Jisan <shihabunjisan@gmail.com>
2023-06-23 19:37:31 +0600

    Included Filter Feature to Category

---
+++ js/data.js
@@ -0,0 +1,116 @@
+// all menu items
+export const menu = [
+  {
+    id: 1,
+    title: "beef burger",
+    category: "fast-food",
+    price: 200,
+    img: "./images/category-recipes/burger-2-darucini-menu-min.jpg",
+    desc: `The classic burger, made with ground beef and grilled to
+    perfection.`,
+  },
+  {
+    id: 2,
+    title: "chicken tikka",
+    category: "fast-food",
+    price: 160,
+    img: "./images/category-recipes/chicken-tikka-2-min.jpg",
+    desc: `Made with chunks of chicken that have been marinated in a
+    yogurt-based sauce with spices`,
+  },
+  {
+    id: 3,
+    title: "gourmet burger",
+    category: "fast-food",
+    price: 220,
+    img: "./images/category-recipes/burger-darucini-menu-min.jpg",
+    desc: `A burger made with high-quality ingredients, such as Angus beef,
+    bacon, and cheese.`,
+  },
+  {
+    id: 4,
+    title: "batonnet",
+    category: "fast-food",
+    price: 290,
+    img: "./images/category-recipes/french-fry-darucini-menu-min.jpg",
+    desc: `These are the classic French fries, cut into long, thin sticks.`,
+  },
+  {
+    id: 5,
+    title: "fried chicken + dip",
+    category: "fast-food",
+    price: 320,
+    img: "./images/category-recipes/Fried-Chicken-With-Dip-min.jpg",
+    desc: `Golden and crispy, fried chicken is a treat.`,
+  },
+  {
+    id: 6,
+    title: "sweet potato fries",
+    category: "fast-food",
+    price: 120,
+    img: "./images/category-recipes/French-fry-darucini-menu2-min.jpg",
+    desc: `Made from sweet potatoes instead of regular potatoes.`,
+  },
+  {
+    id: 7,
+    title: "vegetarian pizza",
+    category: "pizza",
+    price: 420,
+    img: "./images/category-recipes/pizza-darucini-menu-min.jpg",
+    desc: ` Opped with a variety of vegetables, including mushrooms, onions,
+    peppers, and olives.`,
+  },
+  {
+    id: 8,
+    title: "pasta indian",
+    category: "pasta",
+    price: 550,
+    img: "./images/category-recipes/pasta-restaurent-menu-min.jpg",
+    desc: `Short, tube-shaped noodles that are often served with meat sauce
+    or baked pasta dishes.`,
+  },
+  {
+    id: 9,
+    title: "rotini pasta",
+    category: "pasta",
+    price: 290,
+    img: "./images/category-recipes/Pasta-Image-min.jpg",
+    desc: `Spiral-shaped noodles that are often served with pesto or
+    marinara sauce.`,
+  },
+  {
+    id: 10,
+    title: "stick noodles",
+    category: "noodles",
+    price: 80,
+    img: "./images/category-recipes/Noodles-min.jpg",
+    desc: `A type of noodle made from rice flour and water.`,
+  },
+  {
+    id: 11,
+    title: "orange juice",
+    category: "drinks",
+    price: 130,
+    img: "./images/category-recipes/juice-darucini-menu-min.jpg",
+    desc: `A good source of vitamin C, potassium, and other nutrients.
+    Orange juice is often served chilled and can be enjoyed.`,
+  },
+  {
+    id: 12,
+    title: "sugar cones",
+    category: "ice-cream",
+    price: 90,
+    img: "./images/category-recipes/ice-cream-darucini-menu-min.jpg",
+    desc: `Made with a simple sugar dough and are characterized by their
+    light, crispy texture.`,
+  },
+  {
+    id: 13,
+    title: "paneer tikka",
+    category: "fast-food",
+    price: 200,
+    img: "./images/category-recipes/chicken-tikka-min.jpg",
+    desc: `This is a vegetarian option, and is made with cubes of paneer
+    cheese`,
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,9 +1,106 @@
-const toggleBtn = document.querySelector(".toggle_btn");
-const toggleBtnIcon = document.querySelector(".toggle_btn i");
-const dropDownMenu = document.querySelector(".dropdown_menu");
+import { menu } from "./data.js";
 
-toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle("open");
-  const isOpen = dropDownMenu.classList.contains("open");
-  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-};
+// selection menuwrapper & btnwrapper
+const menuWrapper = document.querySelector(".category-items");
+const categoryWrapper = document.querySelector(".category");
+
+// after the window is loaded
+window.addEventListener("DOMContentLoaded", () => {
+  displayMenuItems(menu);
+  displayMenuButtons();
+
+  // when the window is loaded add active class to the category link all
+  const menuBtnAll = document.getElementById("all");
+  menuBtnAll.classList.add("active");
+});
+// display category items
+function displayMenuItems(menuItems) {
+  const displayMenu = menuItems
+    .map((item) => {
+      return `<div class="img-content-container">
+      <img
+        class="card"
+        src=${item.img}
+      />
+      <div class="content">
+        <h1>${item.title}</h1>
+        <p>
+          ${item.desc}
+        </p>
+      </div>
+    </div>`;
+    })
+    .join("");
+  menuWrapper.innerHTML = displayMenu;
+}
+// display category buttons
+function displayMenuButtons() {
+  // get unique categories
+  const categories = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  // add all categories into dom
+  const displayCategory = categories
+    .map((category) => {
+      return `<a class="btn btn-light filter-btn" id=${category} data-id=${category}>${
+        category.charAt(0).toUpperCase() + category.slice(1)
+      }</a>`;
+    })
+    .join("");
+  categoryWrapper.innerHTML = displayCategory;
+
+  // filter menu items
+  const links = categoryWrapper.querySelectorAll(".filter-btn");
+  links.forEach((link) => {
+    // after clicking a button
+    link.addEventListener("click", (e) => {
+      // user selected category
+      const selectedCategory = e.currentTarget.dataset.id;
+
+      // get category button with user selection
+      const categoryBtn = document.getElementById(selectedCategory);
+
+      // Remove Link active class from all other buttons
+      links.forEach((link) => {
+        if (link.classList.contains("active")) {
+          link.classList.remove("active");
+        }
+        // Add Active class to the selected link
+        categoryBtn.classList.add("active");
+      });
+
+      // filter menu with user selected category
+      const filteredCategory = menu.filter((item) => {
+        if (item.category === selectedCategory) {
+          return item;
+        }
+      });
+      // load menu items via selection
+      if (selectedCategory === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(filteredCategory);
+      }
+    });
+  });
+}
+
+// const questions = document.querySelectorAll(".question");
+
+// questions.forEach((question) => {
+//   const btn = question.querySelector(".question-btn");
+//   btn.addEventListener("click", () => {
+//     questions.forEach((item) => {
+//       if (item !== question) {
+//         item.classList.remove("show-text");
+//       }
+//     });
+//     question.classList.toggle("show-text");
+//   });
+// });
